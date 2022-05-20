1f672ba2da3c7516519c82b1132d40eaadb65179
Anh Vuong <vta19797@gmail.com>
2022-05-19 21:09:16 -0500

    add-favorite.js

--- public/javascript/add-favorite.js
+++ public/javascript/add-favorite.js
@@ -1,21 +1,23 @@
 // Function to add favorite
 async function addFavorite (event){
     event.preventDefault();
+   
+    const user_id = 1;
+    const chat_id  = 5;
 
-    const console_id  = document.querySelector('select[name="addFavorite"]').value
-
-    const response = await fetch('/favorite', {
+    const response = await fetch('/api/favorites', {
         method: 'post',
         body: JSON.stringify({
-            username
-           ,password
+            user_id
+           ,chat_id
         }),
         headers: { 'Content-Type': 'application/json' }
     });
     // Check the response status
     if (response.ok) {
-        document.location.replace('/dashboard');
+        document.location.replace('/favorites');
     } else {
-        alert("User or password invalid");
+        alert("Unable to add this chat to favorite");
     };
 };
+document.querySelector('#addFavorite').addEventListener('click', addFavorite);
